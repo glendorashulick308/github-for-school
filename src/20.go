b4c49d77707ec74c4358ed6fa130fceb6ee5124f
glendorashulick308 <glendorashulick308@gmail.com>
2025-03-30 07:55:08 +0000

    Add 20.go

---
+++ src/20.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Example Go code
+    var x int = 5
+    if x > 0 {
+        fmt.Println("x is greater than zero")
+    } else {
+        fmt.Println("x is not greater than zero")
+    }
+}
